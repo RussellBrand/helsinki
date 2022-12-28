f8cb5cdc2dcde57b800f80ef3637b553842df0f6
Russell Brand <brand@responsible.com>
2022-12-28 05:33:24 -0800

    Finish 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -36,25 +36,31 @@ const Total = (props) => {
 const App = () => {
   const course = "Half Stack application development";
 
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
 
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
 
-  const part3 = { name: "State of a component", exercises: 14 };
+    { name: "State of a component", exercises: 14 },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total total={sum(parts)} />
     </div>
   );
 };
+
+function sum(parts) {
+  return parts.map(({ exercises }) => exercises).reduce((a, b) => a + b);
+}
 
 export default App;
