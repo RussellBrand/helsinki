669d4874f4f5f03e00be3e163e36c2523331cb63
Russell Brand <brand@responsible.com>
2022-12-28 18:17:29 -0800

    Finish CourseInfo through 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,72 +1,51 @@
-const Header = (props) => {
-  const { course } = props;
-  return <h1> {course} </h1>;
-};
-
-const Section = (props) => {
-  const { name, exercises } = props;
-  return (
-    <p>
-      <b>{name}: </b>
-      {exercises}
-    </p>
-  );
-};
-
-const Content = (props) => {
-  const { parts } = props;
-  return (
-    <div>
-      {parts.map(({ id, name, exercises }) => {
-        return <Section key={id} name={name} exercises={exercises} />;
-      })}
-    </div>
-  );
-};
-
-const Total = (props) => {
-  const { total } = props;
-  return (
-    <p>
-      <b> Number of exercises:</b> {total}
-    </p>
-  );
-};
-
+import Courses from "./Courses.jsx";
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-    parts: [
-      {
-        id: 1,
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-
-      { id: 2, name: "Using props to pass data", exercises: 7 },
-
-      { id: 3, name: "State of a component", exercises: 14 },
-    ],
-  };
-
-  return <Course course={course} />;
+  return <Courses courses={courses} curriculum="Web Development Curriculum" />;
 };
-
-const Course = (props) => {
-  const { course } = props;
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total total={sum(course.parts)} />
-    </div>
-  );
-};
-
-function sum(parts) {
-  return parts.map(({ exercises }) => exercises).reduce((a, b) => a + b);
-}
 
 export default App;
